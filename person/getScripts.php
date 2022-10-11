6a36faefddd868fc8abe40183fa2bb4d9db726c6
Roman Yarokhin <yaroxin@yandex.ru>
2022-10-11 14:41:47 +0300

    dev_0.1_start

--- person/getScripts.php
+++
@@ -1,3 +0,0 @@
-<script src="http://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-
-<script type="text/javascript" src="script/main.js"></script>
